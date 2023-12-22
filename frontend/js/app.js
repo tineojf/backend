56bc45c65574d2f1ace8a33f3d0b8c4f35319f40
tineojf <92743762+tineodev@users.noreply.github.com>
2023-12-22 08:55:41 -0500

    Update style and functionality of note cards

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,20 +1,60 @@
 import { API_URL } from './url.js';
 
 const API_NOTES = API_URL + 'notes/';
+const listaNotes = document.getElementById('list-notes');
 
 async function getNotes() {
-  const response = await fetch(API_NOTES);
-  const notes = await response.json();
-  return notes;
+  try {
+    const response = await fetch(API_NOTES);
+
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+
+    const notes = await response.json();
+    return notes;
+  } catch (error) {
+    console.error('Error al obtener notas:', error);
+    throw error;
+  }
 }
 
 async function main() {
-  const notes = await getNotes();
-  console.log(notes);
+  try {
+    const notes = await getNotes();
 
+    listaNotes.innerHTML = '';
 
+    notes.notes.forEach(nota => {
+      const notaHTML = `
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <p class="card-text">
+              ${nota.description}
+            </p>
+            <p class="card-subtitle mb-2 text-muted txt-time">${dateConverter(nota.createdAt)}</p>
+            <a href="#" class="card-link btn-archive">Archive</a>
+            <a href="#" class="card-link btn-delete">Delete</a>
+          </div>
+        </div>
+      `;
 
+      listaNotes.innerHTML += notaHTML;
+    });
+  } catch (error) {
+    console.error('Error en la aplicación:', error);
+  }
+}
 
+function dateConverter(fechaISO) {
+  const dateUTC = new Date(fechaISO);
+
+  const dateLocal = dateUTC.toLocaleDateString();
+  const hourLocal = dateUTC.toLocaleTimeString();
+
+  const formatedDate = `${dateLocal} ${hourLocal}`;
+
+  return formatedDate;
 }
 
 main();
