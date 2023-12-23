cee91ab54a0e64b1b2480946ba519b7b9a709c7e
tineojf <92743762+tineodev@users.noreply.github.com>
2023-12-23 17:51:36 -0500

    Update noteDetail.css and noteDetail.js

--- frontend/js/noteDetail.js
+++ frontend/js/noteDetail.js
@@ -8,24 +8,10 @@ const API_NOTES = API_URL + 'notes/';
 const listaNotes = document.getElementById('list-notes');
 
 const h1TitleNote = document.querySelectorAll('.h1-title-note');
+const btnArchive = document.querySelector('.btn-archive');
+console.log(btnArchive);
 
 async function getNoteID(noteId) {
-  try {
-    const response = await fetch(API_NOTES + noteId);
-
-    if (!response.ok) {
-      throw new Error(`Error en la solicitud: ${response.status}`);
-    }
-
-    const notes = await response.json();
-    return notes;
-  } catch (error) {
-    console.error('Error al obtener notas:', error);
-    throw error;
-  }
-}
-
-async function updateArchiveNote(noteId) {
   try {
     const response = await fetch(API_NOTES + noteId);
 
@@ -51,37 +37,67 @@ async function main() {
     })
 
     const notaHTML = `
-      <div class="card" style="width: 18rem">
-        <div class="card-body">
-          <p class="card-text">${nota.description}</p>
-          <p class="card-subtitle mb-2 text-muted txt-time">
-            Created At: ${dateConverter(nota.createdAt)}
-          </p>
-          <p class="card-subtitle mb-2 text-muted txt-time">
-            Updated At: ${dateConverter(nota.createdAt)}
-          </p>
-          <p class="card-text">Archive?: ${(nota.isArchive) ? 'Yes' : 'No'}</p>
-          <div>
-            <a href="" class="card-link btn-archive" id="btnArchive-${nota.id}"
-            >Archive</a
-            >
-            <a href="" class="card-link btn-update" id="btnUpdate-${nota.id}"
-              >Update</a
-            >
-            <a href="" class="card-link btn-delete" id="btnDelete-${nota.id}"
-              >Delete</a
-            >
-          </div>
+      <div class="card">
+        <p class="card-text">${nota.description}</p>
+        <p class="card-subtitle mb-2 text-muted txt-time">
+          Created At: ${dateConverter(nota.createdAt)}
+        </p>
+        <p class="card-subtitle mb-2 text-muted txt-time">
+          Updated At: ${dateConverter(nota.createdAt)}
+        </p>
+        <p class="card-text txtArchive">Archive note?: ${(nota.isArchive) ? 'Yes' : 'No'}</p>
+        <div>
+          <a href="" class="card-link btn-archive" id="btnArchive-${nota.id}"
+          >Archive</a
+          >
+          <a href="" class="card-link btn-update" id="btnUpdate-${nota.id}"
+            >Update</a
+          >
+          <a href="" class="card-link btn-delete" id="btnDelete-${nota.id}"
+            >Delete</a
+          >
         </div>
       </div>
       `;
 
     listaNotes.innerHTML = notaHTML;
+
   } catch (error) {
     console.error('Error en la aplicación:', error);
   }
 }
 
+btnArchive.addEventListener('click', async (e) => {
+  e.preventDefault();
 
+  const notes = await updateNote();
+  console.log(notes);
+  main();
+});
+
+async function updateNote() {
+  try {
+    const response = await fetch(API_NOTES + noteId, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        description: document.querySelector('.card-text').value,
+        isArchive: true,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+
+    const notes = await response.json();
+    return notes;
+  } catch (error) {
+    console.error('Error al obtener notas:', error);
+    throw error;
+  }
+}
 
 main();
